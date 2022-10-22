23767184cbe74710e8801ca8417f1e06d4b1f5ed
thekoushikdurgas <thekoushikdurgas@gmail.com>
2022-10-22 17:13:12 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,17 +1,4 @@
-// $host = "106.75.214.152";
-// $port = 8080;
-
-// set_time_limit(0);
-
-// $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket");
-
-// $result = socket_connect($socket, $host, $port);
-
-
-// $response = socket_read($socket, 2048) or die("Failed reading data");
-// echo $response;
 <?php
 header('Access-Control-Allow-Origin: http://localhost:3000');
 $user = $_POST['url'];
 echo ("Hello from server: $user");
-?>
