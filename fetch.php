032dfa75c5533237cd0999a80569dbfeb4bbc581
thekoushikdurgas <thekoushikdurgas@gmail.com>
2022-10-22 17:42:57 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,5 +1,5 @@
 <?php
-// header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: http://localhost:3000');
 $user = $_POST['url'];
 echo ("Hello from server: $user");
 ?>
